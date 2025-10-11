2ccefbc5e15ca22c4575c97bf16d65e935790796
Chau Thi <dinhchauthi@vnpt.vn>
2025-10-11 13:12:01 +0700

    fix: update function check existed payment

--- notification-service/src/main/java/com/pbl/elearning/notification/payload/request/RegisterDeviceRequest.java
+++ notification-service/src/main/java/com/pbl/elearning/notification/payload/request/RegisterDeviceRequest.java
@@ -3,18 +3,12 @@ package com.pbl.elearning.notification.payload.request;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.pbl.elearning.notification.domain.enums.Platform;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-
-import javax.validation.constraints.NotBlank;
+import lombok.Data;
 import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
-@Getter
-@Setter
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-@Builder
+@Data
 public class RegisterDeviceRequest {
     private UUID userId;
     @NotNull private String deviceToken;
