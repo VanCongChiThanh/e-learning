8d984696701fed8dc079c7ca0bb5c7621b29223e
Chau Thi <dinhchauthi@vnpt.vn>
2025-10-11 13:24:52 +0700

    fix: revert

--- notification-service/src/main/java/com/pbl/elearning/notification/payload/request/RegisterDeviceRequest.java
+++ notification-service/src/main/java/com/pbl/elearning/notification/payload/request/RegisterDeviceRequest.java
@@ -3,12 +3,18 @@ package com.pbl.elearning.notification.payload.request;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.pbl.elearning.notification.domain.enums.Platform;
-import lombok.Data;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
+@Getter
+@Setter
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-@Data
+@Builder
 public class RegisterDeviceRequest {
     private UUID userId;
     @NotNull private String deviceToken;
