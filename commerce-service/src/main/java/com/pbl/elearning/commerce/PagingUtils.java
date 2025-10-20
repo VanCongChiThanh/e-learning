0cc3701f58fa9b9dae097d2aa2176ab2fbc0c673
Chau Thi <dinhchauthi@vnpt.vn>
2025-10-20 20:32:27 +0700

    feat: add paging utils again

---
+++ commerce-service/src/main/java/com/pbl/elearning/commerce/PagingUtils.java
@@ -0,0 +1,33 @@
+package com.pbl.elearning.commerce;
+
+import org.apache.commons.text.CaseUtils;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+public final class PagingUtils {
+
+  public static final String ASC = "asc";
+
+  public static final String DESC = "desc";
+
+  private PagingUtils() {
+  }
+
+  /**
+   * Handle make page request for query
+   *
+   * @param sortBy Sort By Field
+   * @param order  Order By Desc Or Asc
+   * @param page   Page No
+   * @param paging Page Size
+   * @return Pageable
+   */
+  public static Pageable makePageRequest(String sortBy, String order, int page, int paging) {
+    String sortField = CaseUtils.toCamelCase(sortBy, false, '_');
+    Sort sort = ASC.equalsIgnoreCase(order)
+        ? Sort.by(sortField).ascending()
+        : Sort.by(sortField).descending();
+    return PageRequest.of(page - 1, paging, sort);
+  }
+}
