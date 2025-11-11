0202a88872769c0a7210a08f31b956529905cc05
Văn Công Chí Thanh <vancongchithanh2004@gmail.com>
2025-11-11 17:00:48 +0700

    unit test

--- user-service/src/main/java/com/pbl/elearning/user/payload/request/instructor/ApplyInstructorRequest.java
+++ user-service/src/main/java/com/pbl/elearning/user/payload/request/instructor/ApplyInstructorRequest.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.validator.constraints.URL;
 
 import javax.validation.constraints.NotBlank;
 
@@ -14,7 +15,13 @@ import javax.validation.constraints.NotBlank;
 public class ApplyInstructorRequest {
     @NotBlank
     private String cvUrl;
+
+    @NotBlank
+    @URL(message = "portfolio_link must be a valid URL")
     private String portfolioLink;
+
+    @NotBlank
     private String motivation;
+
     private JsonNode extraInfo;
 }
