034705c5823fc11917d8ab902fd448742b8a36a9
Văn Công Chí Thanh <vancongchithanh2004@gmail.com>
2025-12-04 09:04:25 +0700

    update: instructor application

--- enrollment-service/src/main/java/com/pbl/elearning/enrollment/repository/RevenueRepository.java
+++ enrollment-service/src/main/java/com/pbl/elearning/enrollment/repository/RevenueRepository.java
@@ -1,6 +1,6 @@
 package com.pbl.elearning.enrollment.repository;
 
-import com.pbl.elearning.user.domain.UserInfo;
+import com.pbl.elearning.enrollment.models.Enrollment;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -9,7 +9,7 @@ import java.time.Instant;
 import java.util.List;
 import java.util.UUID;
 @Repository
-public interface RevenueRepository extends JpaRepository<UserInfo, UUID> {
+public interface RevenueRepository extends JpaRepository<Enrollment, UUID> {
 
     @Query(value = """
     SELECT
